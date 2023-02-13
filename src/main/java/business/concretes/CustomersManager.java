cc135c5a1ae42ad6366679f108fc6461bb27a54d
Cemil <okcuoglucemil@gmail.com>
2023-02-13 14:09:26 +0300

    costumer manager

--- src/main/java/business/concretes/CustomersManager.java
+++ src/main/java/business/concretes/CustomersManager.java
@@ -1,56 +1,114 @@
 package business.concretes;
 
-import core.helpers.IdMaker;
+import business.abstracts.CustomerService;
+import core.helpers.GetReservation;
 import core.helpers.Slow;
-import core.validations.AgeValidator;
-import core.validations.IdValidator;
+import core.validations.DateValidator;
+import core.validations.DriverLicenceValidator;
 import core.validations.NameValidator;
+import core.validations.TcNoValidator;
 import entities.concretes.Customers;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class CustomersManager extends Customers  {
+public class CustomerManager extends CustomerService {
 
 
+//Müsteri ile ilgili id, ad, soyad, dogum tarihi, ehliyet alis tarihi ve telefon bilgiler
 
-    Customers customers = new Customers();
+    Customers customer = new Customers();
+    MenuManager menuManager = new MenuManager();
+    public List<Customers> customersNewList = new ArrayList<>();
+    public static int counter = 1000;
 
     //Validations
-    AgeValidator ageValidator = new AgeValidator();
-    IdValidator idValidator = new IdValidator();
     NameValidator nameValidator = new NameValidator();
+    TcNoValidator tcNoValidator = new TcNoValidator();
+    DateValidator dateValidator = new DateValidator();
+    DriverLicenceValidator driverLicenceValidator = new DriverLicenceValidator();
+    ReservationManager reservationManager = new ReservationManager();
+    GetReservation reservationCard = new GetReservation();
+    Scanner scanner = new Scanner(System.in);
 
-    Scanner inp = new Scanner(System.in);
+    @Override
+    public void register() {
+        System.out.println("================================== MÜSTERI KAYIT =============================================\n");
+        System.out.println("Müşteri kayıt bölümüne hoşgeldiniz");
+        System.out.println("=================================================================================================");
 
-    public List<Customers> registeredCustomerList = new ArrayList<>();
-
-    public void register(){
-        System.out.println("Müşteri kayıt bölümüne hoşgeldiniz !");
-        System.out.println("====================================");
         System.out.println("Lütfen adınızı giriniz: ");
-        customers.setFirstName(nameValidator.isValidFirstName());
+        customer.setFirstName(nameValidator.isValidFirstName());
+
         System.out.println("Lütfen soyadınızı giriniz: ");
-        customers.setLastName(nameValidator.isValidLastName());
-        System.out.println("Lütfen yaşınızı giriniz: ");
-        customers.setAge(ageValidator.isValidAge());
+        customer.setLastName(nameValidator.isValidLastName());
+
         System.out.println("Lütfen kimlik numaranızı giriniz: ");
-        customers.setTcNo(idValidator.isValid());
+        customer.setTcNo(tcNoValidator.getValidTcNumber());
 
-        customers.setId(idMaker(customers.getTcNo()));
-        addCustomer();
-        counter++;
-        System.out.println("Başarıyla eklenmiştir...");
-        String s ="Rezervasyon onay bölümüne yönlendiriliyorsunuz...\n";
-        Slow.slowPrint(s,30);
-        System.out.println(registeredCustomerList);
+        System.out.println("Lütfen dogum tarihinizi giriniz: ");
+        customer.setBirthDate(dateValidator.getValidDate());
+
+        System.out.println("Lütfen ehliyetinizin verilis tarihinizi giriniz: ");
+        customer.setDriverLicenceDate(dateValidator.getValidDate());
+
+        System.out.println();
+
+        String message1 ="Kiralama icin bilgileriniz kontrol ediliyor............";
+        Slow.slowPrint(message1,60);
+        System.out.println();
+        boolean driverLicenceValid = driverLicenceValidator.driverLicenceEligibility(customer.getBirthDate(), customer.getDriverLicenceDate());
+
+        if (driverLicenceValid) {
+
+            customer.setId(idMaker(customer.getTcNo()));
+
+            counter++;
+            System.out.println();
+            System.out.println(customer);
+            System.out.println();
+            System.out.println("Müsteri Başarıyla eklenmiştir...\n");
+            reservationCard.addCustomerToList(customer);
+            System.out.println();
+
+            String s = "===> Rezervasyon onay bölümüne yönlendiriliyorsunuz...\n";
+            Slow.slowPrint(s, 30);
+            System.out.println();
+            reservationManager.reservationConfirmation();
+        } else {
+
+            System.out.println("Bilgileriniz arac kiralama icin uygun görülmemistir!");
+
+            System.out.println("========================================== !!!!! ==========================================");
+            String message2 = "===> Arac secim bölümüne tekrardan yönlendiriliyorsunuz! \n";
+            Slow.slowPrint(message2, 30);
+            menuManager.getSelectionMenu();
+        }
     }
 
-    public void addCustomer(){
-        Customers customers1 = new Customers(customers.getTcNo(),customers.getAge(),customers.getId(),customers.getFirstName(),customers.getLastName());
-        registeredCustomerList.add(customers1);
+    @Override
+    public void addCustomerToNewList() {
+
+        customersNewList.add(customer);
     }
+
+    @Override
+    public String idMaker(String number) {
+
+        String suffix = "CUST";
+
+        number = number.substring(number.length() - 3);
+
+        return suffix + number + counter; //CUST1231000
+    }
+
+
+    //Id olustur (Markaninilkharfi + Modelin ilk karakteri + model yili + sayac) ++
+    //Müsteri ile ilgili id, ad, soyad, dogum tarihi, ehliyet alis tarihi ve telefon bilgilerini gir ++
+    //dogrulamlari kontrol et ++
+    //animasyon ++
+    //rezervasyon onayla kismina git ++
 
 
 }
