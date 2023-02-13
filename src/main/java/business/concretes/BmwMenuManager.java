c20ac018b16c39222e6b1ab6f8d5dcb466a42440
Cemil <okcuoglucemil@gmail.com>
2023-02-13 14:08:31 +0300

    audi menu manager

--- src/main/java/business/concretes/BmwMenuManager.java
+++ src/main/java/business/concretes/BmwMenuManager.java
@@ -1,40 +1,120 @@
 package business.concretes;
 
-import business.abstracts.MenuService;
+import application.console.concretes.Start;
+import business.abstracts.CarService;
 import core.helpers.GetCar;
 import core.helpers.Slow;
 import entities.concretes.Bmw;
+import entities.concretes.Honda;
 
-import java.awt.*;
 import java.util.Scanner;
 
-public class BmwMenuManager extends MenuManager {
+public class BmwMenuManager extends MenuManager implements CarService {
 
     Scanner inp = new Scanner(System.in);
-
-    ReservationManager reservationManager = new ReservationManager();
+    Bmw bmw = new Bmw(); //DI (Dependeny Injection) --> Don't repeat yourself (DRY)  - SOLID
+    CustomerManager customerManager = new CustomerManager();
     GetCar getCar = new GetCar();
 
-    CustomersManager customersManager = new CustomersManager();
-
-    Bmw bmw = new Bmw();
-
-    public void bmwMenu(){
-        bmw.fillBmwList();
+    public void bmwMenu() {
         String select;
-        bmw.showBmwCars();
-        System.out.println("========================");
+
+        bmw.fillBmwList();
+        bmw.showAvailableCars(true);
+
+//        System.out.println("=================== InactiveCars ==============");
+//        bmw.showInactiveCars(false);
+
+        System.out.println("================================== ARAC SECIMI =============================================\n");
         System.out.println("Rezervasyon işlemini bitirmek için 'Q', devam etmek için herhangi bir tuşa basınız");
         select = inp.nextLine();
-        if (select.equalsIgnoreCase("Q")){
+
+        if (select.equalsIgnoreCase("Q")) {
             getSelectionMenu();
         }
-        System.out.println("Lütfen kiralamak istediğiniz arabanın kodunu giriniz");
+
+        System.out.print("Lütfen kiralamak istediğiniz arabanın kodunu giriniz:");
+        System.out.println();
         getCar.getBmw();
-        String s ="Müşreti kayıt menüsüne yönlendiriliyorsunuz...\n";
-        Slow.slowPrint(s,30);
-        customersManager.register();
+
+        System.out.println();
+        String s = "==> Müşteri kayıt menüsüne yönlendiriliyorsunuz...\n\n";
+        Slow.slowPrint(s, 30);
+        customerManager.register();
+    }
+
+    @Override
+    public void search() {
+        int flag = 0;
+        String id;
+
+        do {
+            id = inp.nextLine();
+            for (Bmw w : bmw.bmwList) {
+
+                if (w.getId().equals(id)) {
+                    w.showAvailableCars(true);
+                    flag++;
+                    break;
+                }
+            }
+
+            if (flag == 0) {
+                System.out.println("Girilen kodla eşleşen araç bulunamadı!");
+            } else {
+                System.out.println("Başarıyla kaydolmuştur");
+                break;
+            }
+        } while (true);
+    }
+
+
+    public void BmwList() {
+
+        int select;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("Lütfen görüntülemek istediginiz arac listesinin numarasini giriniz");
+        System.out.println("1-Sadece müsait olan araclar");
+        System.out.println("2-Sadece rezerve edilmis araclar");
+        System.out.println("3-Tüm araclar");
+        System.out.println("0-Ana menü");
+        select = inp.nextInt();
+
+        switch (select) {
+            case 1:
+                this.availableCarsList();
+                break;
+            case 2:
+                this.reservedCarsList();
+            case 3:
+                this.allCarsList();
+            case 0:
+                Start.start();
+
+        }
+
+
+    }
+
+
+    @Override
+    public void availableCarsList() {
+        bmw.showAvailableCars(true);
+    }
+
+    @Override
+    public void reservedCarsList() {
+        bmw.showInactiveCars(false);
+    }
+
+    @Override
+    public void allCarsList() {
+        bmw.fillBmwList();
+    }
+
+    @Override
+    public void unAvailableCarsList() {
+
     }
 }
-
-
