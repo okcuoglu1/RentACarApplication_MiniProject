74d595e25d593a50fa6d8b0e34abaae5e3858919
Cemil <okcuoglucemil@gmail.com>
2023-02-13 14:10:26 +0300

    menu manager

--- src/main/java/business/concretes/MenuManager.java
+++ src/main/java/business/concretes/MenuManager.java
@@ -1,56 +1,52 @@
 package business.concretes;
 
+import application.console.concretes.Start;
 import business.abstracts.MenuService;
-import core.helpers.Start;
-import entities.concretes.Honda;
 
 import java.util.Scanner;
 
 public class MenuManager extends MenuService {
 
 
-
-
-
-
-
-
     @Override
     public void getSelectionMenu() {
 
-            HondaMenuManager hondaMenuManager = new HondaMenuManager();
-            AudiMenuManager audiMenuService = new AudiMenuManager();
-            BmwMenuManager bmwMenuService = new BmwMenuManager();
-            Scanner inp = new Scanner(System.in);
-            int select;
-            System.out.println("Lütfen kiralamak istediğiniz aracın markasını giriniz");
-            System.out.println("1-Honda");
-            System.out.println("2-Audi");
-            System.out.println("3-BMW");
-            System.out.println("0-Ana Menü");
-            select = inp.nextInt();
+        HondaMenuManager hondaMenuManager = new HondaMenuManager();
+        AudiMenuManager audiMenuManager = new AudiMenuManager();
+        BmwMenuManager bmwMenuManager = new BmwMenuManager();
 
-            switch (select){
-                case 1:
-                    hondaMenuManager.hondaMenu();
-                    break;
-                case 2:
-                    audiMenuService.audiMenu();
-                    break;
-                case 3:
-                    bmwMenuService.bmwMenu();
-                    break;
-                case 0:
-                    Start.start();
-                default:
-                    System.out.println("Geçersiz Giriş");
-            }
+        int select;
+        Scanner inp = new Scanner(System.in);
 
+        System.out.println("Lütfen kiralamak istediğiniz aracın markasını giriniz");
+        System.out.println("1-Honda");
+        System.out.println("2-Audi");
+        System.out.println("3-BMW");
+        System.out.println("0-Ana menü");
+        select = inp.nextInt();
 
-
-
-
+        switch (select) {
+            case 1:
+                hondaMenuManager.hondaMenu();
+                break;
+            case 2:
+                audiMenuManager.audiMenu();
+            case 3:
+                bmwMenuManager.bmwMenu();
+            case 0:
+                Start.start();
         }
+    }
+
+    @Override
+    public void search() {
 
     }
 
+    @Override
+    public void addCarsNewList() {
+
+
+    }
+
+}
